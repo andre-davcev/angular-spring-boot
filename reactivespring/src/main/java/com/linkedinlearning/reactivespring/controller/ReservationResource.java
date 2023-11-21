cf7a5da85ca5a3fb7f45b134e9ac23fdade8ac8c
Andre Davcev <>
2023-11-21 00:19:49 -0500

    Add reservation controller

---
+++ reactivespring/src/main/java/com/linkedinlearning/reactivespring/controller/ReservationResource.java
@@ -0,0 +1,18 @@
+package com.linkedinlearning.reactivespring.controller;
+
+import com.linkedinlearning.reactivespring.model.Reservation;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping(ReservationResource.ROOM_V_1_RESERVATION)
+@CrossOrigin
+public class ReservationResource {
+    public static final String ROOM_V_1_RESERVATION = "/room/v1/reservation/";
+
+    @GetMapping(path = "{roomId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<Reservation> getReservationById(@PathVariable String roomId) {
+        return null;
+    }
+}

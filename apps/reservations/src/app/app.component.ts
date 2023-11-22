0357219484269662cce57492af4ebe006c990129
Andre Davcev <andre.davcev@gmail.com>
2023-11-22 16:07:24 -0500

    Copy in styling from example

--- apps/reservations/src/app/app.component.ts
+++ apps/reservations/src/app/app.component.ts
@@ -1,14 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'asb-root',
+  imports: [CommonModule, RouterModule, ReactiveFormsModule, HttpClientModule],
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'reservations';
+export class AppComponent implements OnInit {
+  public title: string = 'reservations';
+
+  private baseUrl: string = 'http://localhost:8080';
+  private reservationUrl: string = `${this.baseUrl}/room/v1/reservation/`
+
+  constructor(private http: HttpClient){}
+
+  public rooms!: Room[];
+
+  public ngOnInit(): void {
+    this.rooms = [ new Room("127", "127", "150"),
+    new Room("138", "138", "180"),
+    new Room("254", "254", "200")
+    ];
+  }
+}
+
+export class Room {
+  id: string;
+  roomNumber: string;
+  price: string;
+
+  constructor(id: string, roomNumber: string, price: string) {
+    this.id = id;
+    this.roomNumber = roomNumber;
+    this.price = price;
+  }
+
 }
